dd8d9d20c57c174b06822bdb4a2057c605de5b88
Mfon Ukobo <coache1234@gmail.com>
2024-04-16 22:19:00 +0100

    Add project files.

---
+++ PaySpace.Calculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,55 @@
+﻿using MapsterMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+using PaySpace.Calculator.API.Models;
+using PaySpace.Calculator.Data.Models;
+using PaySpace.Calculator.Services.Abstractions;
+using PaySpace.Calculator.Services.Exceptions;
+using PaySpace.Calculator.Services.Models;
+
+namespace PaySpace.Calculator.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public sealed class CalculatorController(
+        ILogger<CalculatorController> logger,
+        IHistoryService historyService,
+        IMapper mapper)
+        : ControllerBase
+    {
+        [HttpPost("calculate-tax")]
+        public async Task<ActionResult<CalculateResult>> Calculate(CalculateRequest request)
+        {
+            try
+            {
+
+                var result = 0; 
+
+                await historyService.AddAsync(new CalculatorHistory
+                {
+                    Tax = result.Tax,
+                    Calculator = result.Calculator,
+                    PostalCode = request.PostalCode ?? "Unknown",
+                    Income = request.Income
+                });
+
+                return this.Ok(mapper.Map<CalculateResultDto>(result));
+            }
+            catch (CalculatorException e)
+            {
+                logger.LogError(e, e.Message);
+
+                return this.BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("history")]
+        public async Task<ActionResult<List<CalculatorHistory>>> History()
+        {
+            var history = await historyService.GetHistoryAsync();
+
+            return this.Ok(mapper.Map<List<CalculatorHistoryDto>>(history));
+        }
+    }
+}
